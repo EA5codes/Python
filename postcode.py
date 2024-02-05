3b2de13faad15113e14df098fa52562ff004e86f
EsA <ea.avc@outlook.com>
2024-02-05 16:42:14 +0100

    first

---
+++ postcode.py
@@ -0,0 +1,9 @@
+postcode = 8000
+sortedPostcodeList = [1000, 8000, 1250, 4000]
+if postcode not in sortedPostcodeList:
+	print(postcode , " nicht in der Liste")
+else:
+	for code in sortedPostcodeList:
+		if code == postcode:
+			print(postcode, " ist in der Liste")
+			break
